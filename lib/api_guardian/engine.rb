50c064fd13d4c5f09fff77de861722a6f57734dd
Greg Cesnik <greg@twinsunsolutions.com>
2024-03-11 16:06:29 -0500

    remove require

--- lib/api_guardian/engine.rb
+++ lib/api_guardian/engine.rb
@@ -3,7 +3,6 @@
 require 'doorkeeper'
 require 'doorkeeper/jwt'
 require 'doorkeeper/grants_assertion'
-require 'controllers/doorkeeper/application_metal_controller'
 require 'api_guardian/doorkeeper/helpers'
 require 'api_guardian/doorkeeper/otp_validation'
 require 'api_guardian/doorkeeper/jwt_validation'

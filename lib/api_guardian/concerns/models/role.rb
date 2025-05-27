b27d19832727c9b3d1653d74c888a8f8b982e937
gunnarebeling <gunnarebeling@icloud.com>
2025-05-26 19:39:29 -0500

    fixing gem

--- lib/api_guardian/concerns/models/role.rb
+++ lib/api_guardian/concerns/models/role.rb
@@ -15,11 +15,11 @@ module ApiGuardian
           validates :name, uniqueness: true, presence: true
           validates :default, uniqueness: true, if: proc { |r| r.default? }
 
-          scope :default, -> { where(default: true) }
+          scope :default_roles, -> { where(default: true) }
 
           # Class Methods
           def self.default_role
-            default.first
+            default_roles.first
           end
 
           def self.policy_class
